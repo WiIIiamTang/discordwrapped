e1a4d51e5083431b190fbbeedfc28253b733c0f3
William Tang <48343678+WiIIiamTang@users.noreply.github.com>
2023-01-01 17:45:34 -0500

    add preview redirect to endpoint

--- src/routes/(public)/login/+page.server.js
+++ src/routes/(public)/login/+page.server.js
@@ -1,8 +1,20 @@
 import { redirect } from '@sveltejs/kit';
-import { DISCORD_ENDPOINT, PROD_DISCORD_ENDPOINT, RUNTIME_ENV } from '$env/static/private';
+import {
+	DISCORD_ENDPOINT,
+	PROD_DISCORD_ENDPOINT,
+	RUNTIME_ENV,
+	PREVIEW_DISCORD_ENDPOINT
+} from '$env/static/private';
 
 export const actions = {
 	default: () => {
-		throw redirect(303, RUNTIME_ENV === 'PROD' ? PROD_DISCORD_ENDPOINT : DISCORD_ENDPOINT);
+		throw redirect(
+			303,
+			RUNTIME_ENV === 'PROD'
+				? PROD_DISCORD_ENDPOINT
+				: RUNTIME_ENV === 'DEV'
+				? DISCORD_ENDPOINT
+				: PREVIEW_DISCORD_ENDPOINT
+		);
 	}
 };
