eeb9c9830ab9b9774e14e6eef77aa19f169f18c8
William Tang <48343678+WiIIiamTang@users.noreply.github.com>
2023-01-01 15:36:28 -0500

    add random facts while loading

--- src/lib/Spinners/Loading.svelte
+++ src/lib/Spinners/Loading.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { onMount } from 'svelte';
+
 	export let color = '#32266d';
 	export let unit = 'px';
 	export let duration = '1.2s';
@@ -6,6 +8,18 @@
 	export let pause = false;
 	export let moonSize = +size / 7;
 	export let top = +size / 2 - moonSize / 2;
+
+	let fact = '';
+
+	onMount(async () => {
+		console.log('Loading.svelte onMount');
+		const res = await fetch('https://dog-api.kinduff.com/api/facts');
+		const data = await res.json();
+		if (!data.success) {
+			fact = '';
+		}
+		fact = data.facts[0];
+	});
 </script>
 
 <div class="text-slate-500 my-2 text-lg">Loading...</div>
@@ -19,6 +33,7 @@
 		<div class="circle-two" class:pause-animation={pause} />
 	</div>
 </div>
+<div class="text-slate-500 my-8 text-md w-3/5 text-center">{fact}</div>
 
 <style>
 	.wrapper {
